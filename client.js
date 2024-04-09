9a36527248986c3757a6c76b4771519a0914c7d3
Marinna Cesar <marinnagc@al.insper.edu.br>
2024-04-09 09:26:21 -0300

    iniciei

---
+++ client.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+async function get_token(){
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+
+    return axios
+        .post("https://tecweb-js.insper-comp.com.br/token", { username: "marinnagc" }, config)
+        .then((response) => response.data.accessToken);
+}
+
+
+async function get_exercises(token){
+    const config2 = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    }
+    return axios
+        .get("https://tecweb-js.insper-comp.com.br/exercicio", config2)
+        .then((response) => response)
+}
+
+async function main(){
+    let token = await get_token();
+    let exercises = await get_exercises(token);
+    console.log(exercises);
+}
+
+main();
